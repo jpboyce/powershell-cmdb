3d12aa8d0898758cea10f22841680d6623adf258
Jesse Boyce <jesse@jpboyce.org>
2018-02-01 07:46:31 +1000

    Added param code block to Forest function

--- poshcmdb.core.discovery.activedirectory.ps1
+++ poshcmdb.core.discovery.activedirectory.ps1
@@ -4,6 +4,29 @@
 
 #region Get current AD forest
 function Get-MSADForest {
+    Param(
+        [string[]]
+        $forestName
+
+        [string[]]
+        $credentials
+
+        [ValidateSet("Standalone","DomainJoined")]
+        [string[]]
+        $mode = "Standalone"
+
+        [string[]]
+        $server
+    )
+
+
+    if($mode -eq "Standalone"){
+        # If the system is standalone, then we have to connect with explicit details, like domain controller, credentials, etc.
+        $firstForest = Get-ADForest -Credential $credentials -Identity $forestName -Server $server
+    }
+    else{
+        # If the system is domain joined, then we can just use credentials and use the local domain
+    }
     $currentforest = Get-ADForest -Current LocalComputer
 
     <#
@@ -26,3 +49,4 @@ function Get-MSADTrusts {
 }
 #endregion
 
+Get-ADForest -Server
